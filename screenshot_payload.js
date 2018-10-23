7998e2c077ae4a6846479d942d555d4fff3e44e4
Manuel <manuel.mueller@audibene.de>
2018-10-23 12:33:30 +0200

    adding better output aka filename

--- screenshot_payload.js
+++ screenshot_payload.js
@@ -4,13 +4,13 @@ module.exports = async ({ page, context }) => {
   const { url } = context;
   const { fullpage } = context; // values "true" / "false"
   let filename = url.replace(/[^a-z0-9]/gi, '_').toLowerCase();
-
   let short = (new Date().toISOString().slice(0,10)).replace(/\\D/g,'');
+
   await page.setViewport({ width: 1920, height: 1080 });
   await page.goto(url);
   await page.screenshot({ path: './screenshots/'+ filename +'_'+ short +'.png', fullPage: fullpage });
 
-  let data = { this: "worked"}
+  let data = { this: "worked", screenshotname: filename +'_'+ short +'.png'}
 
   return {
     data,
