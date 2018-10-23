d6768700bc8391fe4f6793d5d44e4e38b572f949
Manuel <manuel.mueller@audibene.de>
2018-10-23 12:13:02 +0200

    adding fullpage as context

--- screenshot_payload.js
+++ screenshot_payload.js
@@ -2,11 +2,12 @@
 
 module.exports = async ({ page, context }) => {
   const { url } = context;
+  const { fullpage } = context; // values "yes" / "no"
 
   let short = (new Date().toISOString().slice(0,10)).replace(/\D/g,'');
   await page.setViewport({ width: 1920, height: 1080 });
   await page.goto(url);
-  await page.screenshot({ path: './screenshots/URL_'+short+'.png', fullPage: true });
+  await page.screenshot({ path: './screenshots/'+url+'_'+short+'.png', fullPage: fullpage });
 
   let data = { this: "worked"}
 
