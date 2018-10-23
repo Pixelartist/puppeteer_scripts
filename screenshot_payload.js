09a40bfa6e5bc7c9356a24a4d2abe3aeb52942c3
Manuel <manuel.mueller@audibene.de>
2018-10-23 12:27:54 +0200

    added secure filenames and changed structre of filenaming

--- screenshot_payload.js
+++ screenshot_payload.js
@@ -2,12 +2,13 @@
 
 module.exports = async ({ page, context }) => {
   const { url } = context;
-  const { fullpage } = context; // values "yes" / "no"
+  const { fullpage } = context; // values "true" / "false"
+  let filename = url.replace(/[^a-z0-9]/gi, '_').toLowerCase();
 
-  let short = (new Date().toISOString().slice(0,10)).replace(/\D/g,'');
+  let short = (new Date().toISOString().slice(0,10)).replace(/\\D/g,'');
   await page.setViewport({ width: 1920, height: 1080 });
   await page.goto(url);
-  await page.screenshot({ path: './screenshots/'+url+'_'+short+'.png', fullPage: fullpage });
+  await page.screenshot({ path: './screenshots/'+ filename +'_'+ short +'.png', fullPage: fullpage });
 
   let data = { this: "worked"}
 
